d6c914b25369d45ca9808b14c5453806327cf9f2
Jesus Rosas <jesusr93@outlook.es>
2017-03-10 04:39:21 -0600

    cineApp finished

---
+++ cineApp/src/cineapp/Metodos.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cineapp;
+
+/**
+ *
+ * @author Jesús Rosas
+ */
+public class Metodos {
+    public static final String nombres[] = {"Fernando", "Laura", "Pepe", "Eufrasio"};
+ 
+    public static int generaNumeroEnteroAleatorio(int minimo, int maximo) {
+        int num = (int) (Math.random() * (minimo - (maximo + 1)) + (maximo + 1));
+        return num;
+    }
+
+}
