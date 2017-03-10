da9c7834354b3553a9da766413ada5cb030f90c5
Jesus Rosas <jesusr93@outlook.es>
2017-03-10 04:24:48 -0600

    raicesApp finished

---
+++ raicesApp/src/raicesapp/RaicesApp.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package raicesapp;
+
+/**
+ *
+ * @author Jesús Rosas
+ */
+public class RaicesApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Raices ecuacion=new Raices(1,4,4); //creamos el objeto
+        ecuacion.calcular(); //Calculamos
+    }
+    
+}
