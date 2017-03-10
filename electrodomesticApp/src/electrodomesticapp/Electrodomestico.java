f02f62c0b331eaeedb07805e09e41cb49924877d
Jesus Rosas <jesusr93@outlook.es>
2017-03-10 03:03:09 -0600

    class java electrodomestico available

---
+++ electrodomesticApp/src/electrodomesticapp/Electrodomestico.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package electrodomesticapp;
+
+/**
+ *
+ * @author Erick Cástulo Cóporo
+ */
+public class Electrodomestico {
+    
+}
