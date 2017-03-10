79d7390e4f012cfb25f635df3f2882afb9b5e319
Jesus Rosas <jesusr93@outlook.es>
2017-03-10 04:21:56 -0600

    gameApp finished

---
+++ gamesApp/src/gamesapp/Entregable.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gamesapp;
+
+/**
+ *
+ * @author Jesús Rosas
+ */
+public interface Entregable {
+    public void entregar();
+    public void devolver();
+    public boolean isEntregado();
+    public int compareTo(Object a);
+}
